3ed497e68892a265c853d00c5b10271b83c7e33b
maxim-wq <max2452011@gmil.com>
2023-01-19 08:35:10 +0200

    initial commit

---
+++ dz 21.01.23/main.py
@@ -0,0 +1,32 @@
+import colorama
+def function():
+    pass
+class Human:
+    pass
+cl=colorama
+first=function
+maks=Human
+
+print(colorama.__name__)
+print(cl.__name__)
+print(function.__name__)
+print(first.__name__)
+print(Human.__name__)
+print(maks.__name__)
+print(__name__)
+print(type(1))
+print(type(1.2))
+print(type(2==2))
+print('Здраствуй')
+
+intro_list=[]
+for metod in dir(intro_list):
+    print(metod)
+
+import inspect
+import colorama
+print(inspect.ismodule(colorama))
+print(inspect.isclass(colorama))
+print(inspect.isfunction(colorama))
+print(inspect.getmodule(colorama))
+print(inspect.getmodule(list))
